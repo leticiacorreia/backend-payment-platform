586e759f3e97be9e8b092a967b68c71f41d0c14b
Leticia Correia <leticiacorreiatsi@gmail.com>
2024-09-07 23:27:47 -0300

    configuração do projeto, docker e campos principais

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'cpf',
+        'cnpj',
         'email',
         'password',
+        'wallet',
     ];
 
     /**
